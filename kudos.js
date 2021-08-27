56edd30ed4bc4db922b5042508f9e2359d86ff85
L. Molnár <molnar.laci@outlook.com>
2021-08-27 14:56:24 +0200

    Add files via upload

--- kudos.js
+++ kudos.js
@@ -1,11 +1,16 @@
-javascript: 
-(function () { 
-    var btns = Array.from(document.getElementsByTagName('button')); 
-    btns.forEach(function (btn) { 
-        if (btn.getAttribute('data-testid') == 'kudos_button') { 
-            if (btn.getAttribute('title') != 'View all kudos') { 
-                btn.click(); 
-            } 
-        } 
-    }); 
-})()
+javascript:(
+  function(){
+    var btns=Array.from(document.getElementsByTagName('button'));
+    btns.forEach(function (btn){
+      if(btn.getAttribute('data-testid')=='kudos_button'){
+        ch=btn.children[0].getAttribute('data-testid');
+        if(ch=='unfilled_kudos'){
+          btn.click();}
+          else{
+            console.log('Already clicked!');
+          }
+        }
+      }
+    );
+  }
+)()
